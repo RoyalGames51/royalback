858929836f4a34703771b2a024d6ed18819016ac
JoseBrav <josebravo2015@gmail.com>
2024-10-28 21:23:02 -0300

    firebase

---
+++ src/Controllers/userControllers/getUserByEmail.js
@@ -0,0 +1,21 @@
+const { User,Game } = require('../../database');
+
+module.exports = async(email)=>{
+    try{
+        const usuario = User.findOne({
+            where:{email: email},
+            include: [{
+                model: Game,
+                attributes: ['id', 'name'],
+            }]
+        });
+
+        if(!usuario)
+            throw new Error(`No existe usuario con email ${email} en base de datos.`);
+
+        return usuario;
+
+    }catch(error){
+        throw new Error(`Error al obtener usuario: ${error.message}`);
+    }
+}
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,11 +3,15 @@ const router = Router();
 
 const postUserHandler = require('../Handlers/userHandlers/postUserHandler')
 const deleteUserHandler = require('../Handlers/userHandlers/deleteUserHandler')
+const getUserByEmailHandler = require('../Handlers/userHandlers/getUserByEmailHandler')
 
 
 
 
 router.post('/user/create', postUserHandler);
 router.delete('/user', deleteUserHandler);
+
+
+router.get('/user/email', getUserByEmailHandler);
 
 module.exports= router;
