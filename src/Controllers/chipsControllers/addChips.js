58c8fcc2d17c69e8a0b2b323980ee9e87998f2fd
JoseBrav <josebravo2015@gmail.com>
2024-11-03 12:45:33 -0300

    chipsnumber

--- src/Controllers/chipsControllers/addChips.js
+++ src/Controllers/chipsControllers/addChips.js
@@ -7,7 +7,10 @@ const addChips = async (id, newChips) => {
         if (!userToUpdate) {
             throw new Error("Usuario inexistente");
         }
-
+        const chipsToAdd = Number(newChips);
+        if (isNaN(chipsToAdd)) {
+            throw new Error("El valor de fichas proporcionado no es un número válido");
+        }
         // Suma las nuevas fichas a las fichas actuales del usuario
         const updatedChips = userToUpdate.chips + newChips;
 
