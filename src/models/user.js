5304943b8ca3c4c7dfeb553524438412516e48e2
JoseBrav <josebravo2015@gmail.com>
2024-11-07 18:39:12 -0300

    integer

--- src/models/user.js
+++ src/models/user.js
@@ -40,7 +40,7 @@ module.exports = (sequelize)  => {
             allowNull: true
         },
         chips:{
-            type: DataTypes.STRING,
+            type: DataTypes.INTEGER,
             allowNull: false,
             defaultValue: 0
         },
