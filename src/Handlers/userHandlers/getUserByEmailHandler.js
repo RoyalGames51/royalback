7730991e671c6d50495947d37158fb8e2c4b5b06
JoseBrav <josebravo2015@gmail.com>
2024-11-07 21:23:49 -0300

    cambios

--- src/Handlers/userHandlers/getUserByEmailHandler.js
+++ src/Handlers/userHandlers/getUserByEmailHandler.js
@@ -1,12 +1,8 @@
 const getUserByEmail = require('../../Controllers/userControllers/getUserByEmail');
-
 module.exports = async (req, res) => {
     try {
-        console.log("llega acaaaa");
-        const { email } = req.body;
-        console.log("mailhanlder",email);
+        const { email } = req.query;
         const usuario = await getUserByEmail(email);
-console.log(usuario, "usuariohd");
         res.status(200).json(usuario);
     } catch (error) {
         res.status(400).json(`Error al obtener usuario: ${error.message }`);
--- src/models/user.js
+++ src/models/user.js
@@ -1,56 +1,55 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize)  => {
-     const User= sequelize.define('User', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        nick: {
-            type: DataTypes.STRING(30), 
-             allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-             allowNull: false,
-        },
-        admin: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false, 
-        },
-        avatar:{
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        banned: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-        inactive: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-        age: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        country: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        chips:{
-            type: DataTypes.INTEGER,
+    const User= sequelize.define('User', {
+       id: {
+           type: DataTypes.UUID,
+           defaultValue: DataTypes.UUIDV4,
+           primaryKey: true,
+       },
+       nick: {
+           type: DataTypes.STRING(30), 
             allowNull: false,
-            defaultValue: 0
-        },
-        password: {
-            type:DataTypes.STRING,
-            allowNull: false
-        }
-   },
-
-        { timestamps: false });
+       },
+       email: {
+           type: DataTypes.STRING(50),
+            allowNull: false,
+       },
+       admin: {
+           type: DataTypes.BOOLEAN,
+           defaultValue: false, 
+       },
+       avatar:{
+           type: DataTypes.STRING,
+           allowNull: true
+       },
+       banned: {
+           type: DataTypes.BOOLEAN,
+           defaultValue: false
+       },
+       inactive: {
+           type: DataTypes.BOOLEAN,
+           defaultValue: false
+       },
+       age: {
+           type: DataTypes.INTEGER,
+           allowNull: true
+       },
+       country: {
+           type: DataTypes.STRING,
+           allowNull: true
+       },
+       chips:{
+           type: DataTypes.INTEGER,
+           allowNull: false,
+           defaultValue: 0
+       },
+       password: {
+           type:DataTypes.STRING,
+           allowNull: false
+       }
+  },
+       { timestamps: false });
 
          User.associate = (models) => {
         
