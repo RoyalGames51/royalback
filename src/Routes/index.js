a35c6c6cac2ff87c1e6dd75a036ff3f101189e29
JoseBrav <josebravo2015@gmail.com>
2024-11-10 13:02:49 -0300

    patch

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -22,7 +22,7 @@ const inactiveUserHandler = require('../Handlers/userHandlers/inactiveUserHandle
 
 
 router.get('/getUsers',getAllUserHandler);
-router.put('/actualizar-usuario/:id',updateUserHandler);
+router.patch('/actualizar-usuario/:id',updateUserHandler);
 router.post('/user-create', postUserHandler);
 router.delete('/user-delete/:id', deleteUserHandler);
 router.put('/userAdmin',userAdminHandler);
