40c4af02b0d5e90de10de3020af42b08f5eb52ff
JoseBrav <josebravo2015@gmail.com>
2024-11-10 13:11:31 -0300

    back

--- src/Handlers/userHandlers/updateUserHandler.js
+++ src/Handlers/userHandlers/updateUserHandler.js
@@ -1,14 +1,14 @@
 const updateUser = require('../../Controllers/userControllers/updateUser');
 
 module.exports = async (req, res) => {
-    const { id } = req.params;
+    const { id} = req.params;
+    const  newData  = req.body;
     
     try {
-        // Pasa el `req.body` directamente como `newData`
-        const updUser = await updateUser(id, req.body);
+        const updUser = await updateUser(id, newData);
 
         res.status(200).json(updUser);
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(400).json({ error: error.message })
     }
-};
+}
