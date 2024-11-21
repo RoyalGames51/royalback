54475c6eeb8c067a40e929e58a5fbd8880682e0b
JoseBrav <josebravo2015@gmail.com>
2024-11-21 19:22:26 -0300

    postmods

--- src/Controllers/userControllers/postUser.js
+++ src/Controllers/userControllers/postUser.js
@@ -1,30 +1,46 @@
 const { User } = require("../../database");
 const sendMailHandler = require("../../Mailing/sendMailHandler");
-// const sendMailHandler = require('../../utils/mailing/sendMailHandler');
 
-const postUser = async ( nick, email, password,avatar,age,country,chips ) => {
-    
+const postUser = async (nick, email, password, avatar, age, country, chips) => {
     try {
-     
+        // Convertir el nick a minúsculas
+        const normalizedNick = nick.toLowerCase();
+
+        // Verificar si el nick ya existe en la base de datos
+        const existingNick = await User.findOne({ where: { nick: normalizedNick } });
+        if (existingNick) {
+            throw new Error(`El nick '${nick}' ya está en uso.`);
+        }
+
+        // Crear el usuario en la base de datos
         const [newUser, created] = await User.findOrCreate({
             where: { email },
-            defaults: { nick, email, password,age,avatar,country,chips }
+            defaults: { 
+                nick: normalizedNick, 
+                email, 
+                password, 
+                avatar, 
+                age, 
+                country, 
+                chips 
+            }
         });
-        
-          const register = {
-            nick:nick,
-            email:email,
-            option:"signIn"
-          }
-             
-            await sendMailHandler(register);
-        
-                   
-        return  newUser.dataValues
 
+        // Enviar el correo de bienvenida si se creó el usuario
+        if (created) {
+            const register = {
+                nick: normalizedNick,
+                email,
+                option: "signIn"
+            };
+
+            await sendMailHandler(register);
+        }
+
+        return newUser.dataValues;
     } catch (error) {
-        throw new Error(`Error al crear el usuario (controller): ${error.message}`)
+        throw new Error(`Error al crear el usuario (controller): ${error.message}`);
     }
-}
+};
 
 module.exports = postUser;
