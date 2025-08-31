a0443baa50bc13743267212f4ab7721c3403db1c
royalgames51 <royalgames2025@gmail.com>
2025-08-31 10:50:59 -0300

    db

--- src/database.js
+++ src/database.js
@@ -1,62 +1,57 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { Pool } = require('pg');
-
-
-const fs = require('fs');
-const path = require('path');
-/* const { 
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env; */
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-}); 
-
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rgames`, {
-//   logging: false, 
-//   native: false, 
-// });
+const fs = require("fs");
+const path = require("path");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
-  native: false,
+  dialect: "postgres",
   dialectOptions: {
-       ssl: {
+    ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
   },
 });
+
 const basename = path.basename(__filename);
+const modelDefiners = [];
 
-const modelDefiners = []; 
-
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js"
+  )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
-
-
- modelDefiners.forEach(model => model(sequelize));  
+modelDefiners.forEach((model) => model(sequelize));
 
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map(([name, model]) => [
+  name[0].toUpperCase() + name.slice(1),
+  model,
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { User, Game, Pay} = sequelize.models;
-Pay.belongsTo(User,{through: "UserPays"});
-User.belongsToMany(Pay,{through: "UserPays"});
+const { User, Game, Pay } = sequelize.models;
 
+Pay.belongsTo(User, { through: "UserPays" });
+User.belongsToMany(Pay, { through: "UserPays" });
 
-User.belongsToMany(Game, { through: 'userGames', foreignKey: 'userId' });
-Game.belongsToMany(User, { through: 'userGames', foreignKey: 'gameId' });
-
-
+User.belongsToMany(Game, { through: "userGames", foreignKey: "userId" });
+Game.belongsToMany(User, { through: "userGames", foreignKey: "gameId" });
 
 module.exports = {
-  ...sequelize.models, 
-  conn: sequelize,     
+  ...sequelize.models,
+  conn: sequelize,
 };
